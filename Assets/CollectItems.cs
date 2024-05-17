5ebe3f4243eedb49b8bd7be6d5270dc0c0fb13dc
shashank-8214 <shashankkumarsingh8214@gmail.com>
2024-05-17 17:10:59 +0100

    update audio

--- Assets/CollectItems.cs
+++ Assets/CollectItems.cs
@@ -9,6 +9,9 @@ public class CollectItems : MonoBehaviour
     public TextMeshProUGUI partsCollectedText; // Reference to the TextMeshPro UI element
     private HashSet<GameObject> collectedItems = new HashSet<GameObject>();
 
+    public AudioSource audioSource; // Reference to the AudioSource component
+    public AudioClip collectSound; // Reference to the AudioClip to play
+
     void Start()
     {
         UpdateCollectedItemsDisplay(); // Initial display update
@@ -22,12 +25,21 @@ public class CollectItems : MonoBehaviour
             other.gameObject.SetActive(false); // Hides the GameObject from the scene
             UpdateCollectedItemsDisplay();
 
+            PlayCollectSound(); // Play sound when an item is collected
+
+
             if (collectedItems.Count == 10)
             {
                 allPiecesCollected = true;
                 CompletePuzzle();
             }
         }
+    }
+
+    void PlayCollectSound()
+    {
+        if (audioSource != null && collectSound != null)
+            audioSource.PlayOneShot(collectSound); // Play the collect sound
     }
 
     void UpdateCollectedItemsDisplay()
